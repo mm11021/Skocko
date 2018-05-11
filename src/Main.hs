45380828659f311989f2e7a10a09f051257d7630
mm11021 <mm11021@alas.matf.bg.ac.rs>
2018-05-11 16:37:39 +0200

    Dodata polja sa simbolima za pogadjanje.

--- src/Main.hs
+++ src/Main.hs
@@ -25,28 +25,54 @@ prozor = InWindow "Skocko" (sirina,visina) (pozicija,pozicija)
 boja :: Color
 boja = white
 
-nacrtaj_polje :: Float -> Float -> Float -> Float -> Picture
-nacrtaj_polje x0 y0 x1 y1 = polygon [(x0,y0),(x1,y0),(x1,y1),(x0,y1)]
+--funkcija koja crta pravouganik cija su dijagonalna temena (x0,y0) i (x1,y1)
+nacrtaj_pravougaonik :: Float -> Float -> Float -> Float -> Picture
+nacrtaj_pravougaonik x0 y0 x1 y1 = polygon [(x0,y0),(x1,y0),(x1,y1),(x0,y1)]
 
-tabla :: [Picture]
-tabla = map (\(x,y) -> nacrtaj_polje ((x*vp)-ps) (pv-(y*vp)) ((x+1)*vp-ps) (pv-(y+1)*vp)) [(i,j)| i<-[0,1..3], j<-[0,1..5]]
+--tabla na kojoj se nalaze kombinacije koje igrac pogadja
+tabla :: Picture
+tabla = nacrtaj_pravougaonik (-ps) pv (4*vp-ps) (pv-6*vp)
                   where vp = velicina_polja
                         ps = (fromIntegral sirina) / 2
                         pv = (fromIntegral visina) / 2
 
+--vodoravne linije table
 v_linije :: [Picture]
-v_linije = map (color white . (\x -> line [(-ps,pv-x*vp),(4*vp-ps,pv-x*vp)])) [0,1..6]
+v_linije = take 7 $ map (color white . (\x -> line [(-ps,pv-x*vp),(4*vp-ps,pv-x*vp)])) [0,1..]
                   where vp = velicina_polja
                         ps = (fromIntegral sirina) / 2
                         pv = (fromIntegral visina) / 2
 
+--uspravne linije table
 u_linije :: [Picture]
 u_linije = take 5 $ map (color white . (\x -> line [(x*vp-ps,pv),(x*vp-ps,pv-6*vp)])) [0,1..]
                   where vp = velicina_polja
                         ps = (fromIntegral sirina) / 2
                         pv = (fromIntegral visina) / 2
 
+--tabla na kojoj se nalaze simboli pomocu kojih igrac pogadja
+simboli :: Picture
+simboli = nacrtaj_pravougaonik (5*vp-ps) (2*vp-pv) (8*vp-ps) (-pv)
+                  where vp = velicina_polja
+                        ps = (fromIntegral sirina) / 2
+                        pv = (fromIntegral visina) / 2
 
+--vodoravne linije table simbola
+v_linije_simboli :: [Picture]
+v_linije_simboli = take 3 $ map (color white . (\x -> line [(5*vp-ps,x*vp-pv),(8*vp-ps,x*vp-pv)])) [0,1..]
+                  where vp = velicina_polja
+                        ps = (fromIntegral sirina) / 2
+                        pv = (fromIntegral visina) / 2
+
+--uspravne linije table simbola
+u_linije_simboli :: [Picture]
+u_linije_simboli = take 4 $ map (color white . (\x -> line [((x+5)*vp-ps,-pv),((x+5)*vp-ps,2*vp-pv)])) [0,1..]
+                  where vp = velicina_polja
+                        ps = (fromIntegral sirina) / 2
+                        pv = (fromIntegral visina) / 2
+
+sva_polja :: Picture
+sva_polja = pictures $ tabla : simboli: v_linije ++ u_linije ++ v_linije_simboli ++ u_linije_simboli
 
 main :: IO ()
-main = display prozor boja $ pictures (tabla ++ v_linije ++ u_linije)
+main = display prozor boja sva_polja
